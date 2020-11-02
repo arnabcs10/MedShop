b6aa8299e77ea850caeeaa97c157bea6dad1d16c
arnab <arnab10panda@gmail.com>
2020-11-02 20:04:25 +0530

    Add React-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
     <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' exact component={ProductScreen} />
         </Container>
       </main>
     <Footer />
