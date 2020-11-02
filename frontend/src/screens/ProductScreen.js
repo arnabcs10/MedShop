50c7d946d32d9221e0db5730b1acc8ae4b4eaae0
arnab <arnab10panda@gmail.com>
2020-11-02 22:53:54 +0530

    Fetch products from backend and remove products.js from frontend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import {Link} from 'react-router-dom';
 import { Row,Col,Image,ListGroup,Card,Button, ListGroupItem } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+
 
 const ProductScreen = (props) => {
-    const product = products.find( p => p._id === props.match.params.id);
+    const [product,setProduct] = useState({});
+    useEffect(()=>{
+        const fetchProduct = async () =>{
+            const response = await axios.get(`/api/products/${props.match.params.id}`);
+            setProduct(response.data);
+        }
+        fetchProduct();
+    },[]);
     return (
         <>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
