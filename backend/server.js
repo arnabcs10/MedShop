490c2709aaf9b4adda38eba0b8ec45bb6c36a0bd
arnab <arnab10panda@gmail.com>
2020-11-03 20:39:09 +0530

    Initial database connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const colors = require('colors');
 const products = require('./data/products');
 
 const app = express();
 dotenv.config();
 
+connectDB();
 
 app.get('/',(req,res)=>{
     res.send("Hello world");
@@ -22,5 +25,5 @@ app.get('/api/products/:id',(req,res)=>{
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,() =>{
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
