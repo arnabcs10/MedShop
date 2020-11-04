9ec9cf3824e291bef1b28a75ca921437ba009da9
arnab <arnab10panda@gmail.com>
2020-11-04 13:32:55 +0530

    Fetching products from db and installing express-async-handler

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
+//Import dependencies
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const colors = require('colors');
-const products = require('./data/products');
 
+
+// Import Routes
+const productRoutes = require('./routes/productRoutes');
+
+//App config
 const app = express();
 dotenv.config();
 
+//Database connection
 connectDB();
 
+//Routes
+//Home
 app.get('/',(req,res)=>{
-    res.send("Hello world");
+    res.send("API is running...");
 });
 
-app.get('/api/products/',(req,res)=>{
-    res.json(products);
-});
+//api/products/
+app.use('/api/products/',productRoutes);
 
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find( p => p._id === req.params.id );
-    res.json(product);
-});
+
+
 
 const PORT = process.env.PORT || 5000;
 
