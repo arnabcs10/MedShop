b408b15ad6fcba49072253ee0609f18d2302069c
arnab <arnab10panda@gmail.com>
2020-11-05 14:00:09 +0530

    Initial redux and store setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -8,10 +10,13 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+    </Provider>
+   </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
