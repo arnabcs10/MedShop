8ff39ec9678e54d9bb5d3e07374da52392647461
arnab <arnab10panda@gmail.com>
2020-11-10 19:25:02 +0530

    Resetting orders and user details after logout

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -11,7 +11,8 @@ import {
     ORDER_PAY_RESET,
     ORDER_MY_LIST_REQUEST,
     ORDER_MY_LIST_SUCCESS,
-    ORDER_MY_LIST_FAIL 
+    ORDER_MY_LIST_FAIL ,
+    ORDER_MY_LIST_RESET
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = ( state={}, action)=> {
@@ -89,7 +90,8 @@ export const orderListMyReducer = ( state={ orders:[]}, action)=> {
                 loading: false,
                 error: action.payload
             };
-       
+        case ORDER_MY_LIST_RESET:
+            return { orders:[] };
         default:
             return state;
     }
