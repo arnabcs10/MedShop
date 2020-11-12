0431ba509db297901b5cef37c154176ca7b08cb2
arnab <arnab10panda@gmail.com>
2020-11-12 22:37:39 +0530

    Add reducer actions for order list

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Form, Button} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
@@ -17,17 +18,17 @@ const ProductEditScreen = ({ match, history }) => {
     const [brand, setBrand] = useState('');
     const [category, setCategory] = useState('');
     const [countInStock, setCountInStock] = useState(0);    
-    const [description, setDescription] = useState(0);    
+    const [description, setDescription] = useState('');    
+    const [uploading, setUploading] = useState(false);    
+
 
     
-    
-
     const dispatch = useDispatch();
     const productDetails = useSelector(state => state.productDetails);
     const { loading, error, product } = productDetails;
 
     const productUpdate = useSelector(state => state.productUpdate);
-    const { loading: loadingUpdate, error: errorUpdate, successUpdate } = productUpdate;
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate;
 
     
 
@@ -53,6 +54,27 @@ const ProductEditScreen = ({ match, history }) => {
         
     },[dispatch,productId,product,history,successUpdate]);
 
+    const uploadFileHandler = async (e) =>{
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image', file);
+        setUploading(true);
+
+        try {
+            const config = {
+                headers:{
+                    'Content-Type':"multipart/form-data"
+                }
+            }
+
+            const {data} = await axios.post('/api/upload', formData, config);
+            setImage(data);
+            setUploading(false);
+        } catch (error) {
+            console.error(error);
+            setUploading(false);
+        }
+    }
     const submitHandler = (e) => {
         e.preventDefault();
         
@@ -67,6 +89,7 @@ const ProductEditScreen = ({ match, history }) => {
             description
 
         }));
+        
     }
     return (
         <>
@@ -111,6 +134,13 @@ const ProductEditScreen = ({ match, history }) => {
                             onChange={(e) => setImage(e.target.value)}
                         >
                         </Form.Control>
+                        <Form.File 
+                            id='image-file' 
+                            label='Choose File'
+                            custom
+                            onChange={uploadFileHandler}
+                        ></Form.File>
+                        {uploading && <Loader/>}
                     </Form.Group>
 
                     <Form.Group controlId='brand'>
