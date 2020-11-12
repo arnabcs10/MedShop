fd41658459c22af6fb1383cdffe38f261905d9a6
arnab <arnab10panda@gmail.com>
2020-11-12 14:07:58 +0530

    Add action reducer for update product by admin

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,8 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-import { listProducts, deleteProduct, createProduct } from '../actions/productActions';
-import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
+import { listProducts,  updateProduct } from '../actions/productActions';
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
 
 const ProductEditScreen = ({ match, history }) => {
     const productId = match.params.id;
@@ -26,10 +26,16 @@ const ProductEditScreen = ({ match, history }) => {
     const productDetails = useSelector(state => state.productDetails);
     const { loading, error, product } = productDetails;
 
+    const productUpdate = useSelector(state => state.productUpdate);
+    const { loading: loadingUpdate, error: errorUpdate, successUpdate } = productUpdate;
+
     
 
     useEffect(()=>{
-        
+        if(successUpdate){
+            dispatch({type:PRODUCT_UPDATE_RESET});
+            history.push('/admin/productlist');
+        }else{
             if(!product.name || product._id !== productId){
                 dispatch(listProducts(productId));
             }else{
@@ -41,14 +47,26 @@ const ProductEditScreen = ({ match, history }) => {
                 setCountInStock(product.countInStock)
                 setDescription(product.description)
             }
+        }
+            
         
         
-    },[dispatch,productId,product,history]);
+    },[dispatch,productId,product,history,successUpdate]);
 
     const submitHandler = (e) => {
         e.preventDefault();
         
-        
+        dispatch(updateProduct({
+            _id: productId,
+            name,
+            price,
+            image,
+            brand,
+            category,
+            countInStock,
+            description
+
+        }));
     }
     return (
         <>
@@ -58,7 +76,8 @@ const ProductEditScreen = ({ match, history }) => {
        
         <FormContainer>
             <h1>Edit Product</h1>
-            
+            {loadingUpdate && <Loader />}
+            {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
             { loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message>:(
                     <Form onSubmit={submitHandler}>
                     <Form.Group controlId='name'>
